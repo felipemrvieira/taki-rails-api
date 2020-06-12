d3bc86c7447a8a3e17bb89542df0275873bf31dc
Felipe Maciel Ramos Vieira <felipemrvieira@gmail.com>
2020-06-12 17:17:55 -0300

    Cria metodos dos controlers e exibe relacionamentos

--- app/controllers/businesses_controller.rb
+++ app/controllers/businesses_controller.rb
@@ -1,6 +1,48 @@
 class BusinessesController < ApplicationController
+  protect_from_forgery
+  before_action :set_business, only: [:show, :update, :destroy]
+
   def index
     @businesses = Business.all
-    render json: @businesses
+    render json: @businesses, include: :items
   end
+
+  def show
+    render json: @business, include: :items
+  end
+
+  def create
+    @business = Business.new(business_params)
+    @business.user_id = User.first.id
+
+    if @business.save
+      render json: @business, status: :created, location: @business
+    else
+      render json: @business.errors, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @business.update(business_params)
+      render json: @business
+    else
+      render json: @business.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @business.destroy
+  end
+
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_business
+      @business = Business.find(params[:id])
+    end
+
+    # Only allow a trusted parameter "white list" through.
+    def business_params
+      params.require(:business).permit(:name, :address, :city, :user_id)
+    end
 end
--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,47 @@
 class ItemsController < ApplicationController
+  protect_from_forgery
+  before_action :set_item, only: [:show, :update, :destroy]
+
   def index
     @items = Item.all
-    render json: @items
+    render json: @items, include: :business
   end
+
+  def show
+    render json: @item, include: :business
+  end
+
+  def create
+    @item = Item.new(item_params)
+
+    if @item.save
+      render json: @item, status: :created, location: @item
+    else
+      render json: @item.errors, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @item.update(item_params)
+      render json: @item
+    else
+      render json: @item.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @item.destroy
+  end
+
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_item
+      @item = Item.find(params[:id])
+    end
+
+    # Only allow a trusted parameter "white list" through.
+    def item_params
+      params.require(:item).permit(:name, :price, :business_id)
+    end
 end
